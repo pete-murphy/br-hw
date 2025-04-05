b0396c3da7d2609c8cf7a02eea48959013491ff3
Peter Murphy <26548438+ptrfrncsmrph@users.noreply.github.com>
2025-04-05 03:35:59 -0400

    chore: Cloudflare setup
    TODO: figure out how to use "experimental" sessions and on-demand
    rendering so session IDs are unique to session

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,23 @@ import { defineConfig } from "astro/config"
 import tailwindcss from "@tailwindcss/vite"
 import elmstronaut from "elmstronaut"
 
+import cloudflare from "@astrojs/cloudflare"
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [elmstronaut()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  // experimental: {
+  //   session: true,
+  // },
+
+  // session: {
+  //   driver: "sessionStorage",
+  // },
+
+  adapter: cloudflare(),
 })
